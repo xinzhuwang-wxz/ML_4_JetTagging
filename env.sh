0a8a8b6b1a87d0c144dc4023a4eeb8c54074e0c4
Chin-Zhu Wang <wangxinzhu3141@163.com>
2025-04-16 21:14:44 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_100 x86_64-centos7-gcc8-opt
 
 
-export DATA_DIR="/cefs/higgs/zhuyifan/sample/training/input/input/merge"
-export PATH="/hpcfs/cepc/higgsgpu/wangxinzhu/cepc_hss_scripts/BDTrain/install/bin:$PATH"
+export DATA_DIR="/your path/merge"
+export PATH="/your path'/install/bin:$PATH"
 
 
